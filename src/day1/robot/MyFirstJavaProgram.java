3a72a34f0a0205108d3d530e4f487c166ccd74e0
ThomasTwomey <league@WTS15.attlocal.net>
2017-08-08 11:51:06 -0700

    housed for loops

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
+		Robot myRobot = new Robot();
+		myRobot.sparkle();
+		myRobot.setPenColor(Color.CYAN);
+		myRobot.penDown();
+		myRobot.setSpeed(10);
+		for(int i = 0;i<900;i++)
+		{
+			myRobot.move(9);
+			myRobot.turn(1);
 		
-		
+		}
 	}
 }
