bfcedbe14460f7e6910c7c714a5938864cd0a183
ThomasTwomey <league@WTS15.attlocal.net>
2017-08-09 11:48:51 -0700

    the best code ever

---
+++ src/day3/HelloWorld.java
@@ -0,0 +1,29 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("pi 3.14159265358979323846264338327950288419716939937510582097494459230"
+				+ "78164062862089986280348253421170679");
+		 String name = JOptionPane.showInputDialog("show you name");
+		JOptionPane.showMessageDialog(null,"hi "+ name);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
--- src/day3/StephenHawking.java
+++ src/day3/StephenHawking.java
@@ -2,14 +2,20 @@ package day3;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class StephenHawking {
 
 	// 1. make a main method and put steps 2, 3 & 4 inside it
+	public static void main(String[] args) {
+for (int i = 0; i < 10000000; i++){	
 	// 2. ask the user for a sentence
+	String talk = JOptionPane.showInputDialog("give a sentence");
 	// 3. call the speak method below and send it the sentence
+	speak(talk);
 	// 4. repeat steps 2 and 3 a lot of times
-
-	/* Don’t change this…. */
+}
+	}	/* Don’t change this…. */
 	static void speak(String words) {
 		try {
 			Runtime.getRuntime().exec("say " + words).waitFor();
